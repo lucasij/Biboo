04d237af623ecb3835476619b5db008b9918e8b3
ProcG <you@example.com>
2019-04-13 18:47:38 -0300

    Atualizacao

--- Biboo/biboo/biboo/src/main/java/br/com/biboo/biboo/controler/LoginController.java
+++ Biboo/biboo/biboo/src/main/java/br/com/biboo/biboo/controler/LoginController.java
@@ -6,13 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import br.com.biboo.biboo.infra.SalvarUsuario;
 import br.com.biboo.biboo.infra.Seguranca;
 
 @Controller
 public class LoginController {
 
-	Seguranca seguranca = new Seguranca();
+	private final Seguranca seguranca;
 	
+
+	public LoginController(Seguranca seguranca) {
+		super();
+		this.seguranca = seguranca;
+	}
+
 	@PostMapping("/login")
 	public String efetuarLogin(@ModelAttribute Credenciais credenciais) {
 		if(seguranca.permitirAcesso(credenciais)) return "principal";
@@ -23,5 +30,18 @@ public class LoginController {
 	public String exibirPaginaDeLogin(Model model) {
 		model.addAttribute(new Credenciais(null, null));
 		return "index";
+	}
+	
+	@PostMapping("/cadastro")
+	public String efetuarcadastro(@ModelAttribute Credenciais credenciais) {
+		SalvarUsuario usuario = new SalvarUsuario();
+		usuario.salvarBanco(credenciais);
+		return "principal";
+	}
+	
+	@GetMapping("/cadastro")
+	public String exibirPaginaDeCadastro(Model model) {
+		model.addAttribute(new Credenciais(null, null));
+		return "cadastro";
 	}
 }
